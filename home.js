393ea520118a16eb5394de560acd07d2b13c02a7
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2024-05-13 17:52:56 -0300

    Atualizar o home.js

--- home.js
+++ home.js
@@ -1,72 +1,121 @@
-let configuracao = document.getElementById("config");
-let dispositivo = navigator.userAgent.toLowerCase();
-let appBaixado = localStorage.getItem("app") || false;
 
-let exibirApp = document.getElementById("app");
-let login = localStorage.getItem("login") || false;
-let ativarEmails = localStorage.getItem("AtivarEmails") || true;
-let selectEmail = document.getElementById("AtivarEmails");
-selectEmail.value = ativarEmails;
+let configuracao = document.getElementById("config")
+ let dispositivo = navigator.userAgent
+ dispositivo = dispositivo.toLowerCase()
+  let appBaixado =  localStorage.getItem("app") || false
 
-setInterval(() => {
-  ativarEmails = document.getElementById("AtivarEmails").value;
-  localStorage.setItem("AtivarEmails", ativarEmails);
-}, 100);
+let exibirApp = document.getElementById("app")
+let login = localStorage.getItem("login") || false
+let ativarEmails = localStorage.getItem("AtivarEmails") || true
+let selectEmail = document.getElementById("AtivarEmails")
+selectEmail.value = ativarEmails
+
+setInterval(()=> {
+  
+  ativarEmails = document.getElementById("AtivarEmails").value
+  localStorage.setItem("AtivarEmails",ativarEmails)
+  
+},100)
 
 if (login === false) {
-  alert("Não encontramos seu login...");
-  window.location.href = "index.html";
+  
+  
+  alert("Não encontramos seu login...")
+  window.location.href = "index.html"
 }
+
+
 
 if (dispositivo.includes("android")) {
+  
   if (appBaixado === false) {
-    exibirApp.showModal();
+    
+   exibirApp.showModal()
+    
   }
+  
 }
+
+
 
 function baixar() {
-  appBaixado = true;
-  localStorage.setItem("app", appBaixado);
-  exibirApp.close();
+  
+  appBaixado = true
+  localStorage.setItem("app",appBaixado)
+  exibirApp.close()
+  
+  
 }
-
-function maisTarde() {
-  exibirApp.close();
-}
+ 
+ function maisTarde() {
+   
+   exibirApp.close()
+   
+   
+ }
 
 function config() {
-  configuracao.showModal();
+  
+  configuracao.showModal()
+  
+  
 }
 
 function fecharConfig() {
-  configuracao.close();
+  
+  
+  configuracao.close()
+  
 }
 
 function chat() {
-  window.open("https://www.mediafire.com/file/r9gxb87i5vudhhe/DGX_CHAT_11.6.apk/file");
+  
+  window.open("https://www.mediafire.com/file/r9gxb87i5vudhhe/DGX_CHAT_11.6.apk/file")
+  
 }
 
 function comunidade() {
-  window.open("https://chat.whatsapp.com/HD4ytGwdsWz7RqDRwjTTxQ");
+  
+  window.open("https://chat.whatsapp.com/HD4ytGwdsWz7RqDRwjTTxQ")
+  
 }
 
 function compartilhar() {
-  let url = window.location.href;
-  window.open("https://wa.me/?text=" + encodeURIComponent(url));
+  
+  let url = window.location.href
+window.open("https://wa.me/?text="+url)
+  
+  
 }
 
 function gc() {
-  window.open("https://chat.whatsapp.com/Emm5uCKCiFI7f8pO0QWOcF");
+  
+ window.open("https://chat.whatsapp.com/Emm5uCKCiFI7f8pO0QWOcF")
+  
 }
 
-function crossover() {
-  window.open("https://chat.whatsapp.com/J5AqVzHRggJBnLJpghETob");
+function crossover() { 
+  
+window.open("https://chat.whatsapp.com/J5AqVzHRggJBnLJpghETob")
+  
 }
-
-function cars() {
-  window.open("https://chat.whatsapp.com/DZpzWWkRKQNGX4kkQA9fZI");
-}
-
-window.addEventListener("touchstart", function(event) {
-  event.preventDefault();
-});
+ function cars() {
+   
+   
+window.open("https://chat.whatsapp.com/DZpzWWkRKQNGX4kkQA9fZI")
+   
+ }
+ 
+ 
+ function entrarDicord() {
+   
+   window.open("https://discord.com/invite/aX9tFVEf")
+   
+   
+ }
+ 
+window.addEventListener("contextmenu", function(event) {
+  
+  event.preventDefault()
+  
+})
